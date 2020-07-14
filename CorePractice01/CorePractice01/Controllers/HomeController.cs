eba11ab7645d69bb0e02727912bb3deab0ffeae9
Yuan <mlovyuan@yahoo.com.tw>
2020-07-14 21:48:15 +0800

    2020.07.14_Core_startup

---
+++ CorePractice01/CorePractice01/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using CorePractice01.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorePractice01.Controllers
+{
+    public class HomeController:Controller
+    {
+        // 因為Startup.cs的DI註冊設定，會使用TW_Clock注入
+        public HomeController(IClock clock)
+        {
+
+        }
+    }
+}
---
+++ CorePractice01/CorePractice01/Services/IClock.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorePractice01.Services
+{
+    public interface IClock
+    {
+    }
+}
