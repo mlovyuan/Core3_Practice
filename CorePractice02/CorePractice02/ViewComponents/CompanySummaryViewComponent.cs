379dc6630c1800e5fcb510af0d480d6ed7f10bb1
Yuan <mlovyuan@yahoo.com.tw>
2020-07-27 12:52:29 +0800

    2020.07.27_View Component

---
+++ CorePractice02/CorePractice02/ViewComponents/CompanySummaryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using CorePractice02.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorePractice02.ViewComponents
+{
+    public class CompanySummaryViewComponent : ViewComponent
+    {
+        private readonly IDepartmentService _departmentService;
+
+        public CompanySummaryViewComponent(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(string title)
+        {
+            ViewBag.title = title;
+            var summery = await _departmentService.GetCompanySummary();
+            return View(summery);
+        }
+    }
+}
