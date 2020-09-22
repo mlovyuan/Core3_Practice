f17a5aa7ebdac494065bfbeacbf82b2eddf9b83c
Yuan <mlovyuan@yahoo.com.tw>
2020-09-22 23:07:39 +0800

    2020.09.23_LearningSchedule

--- DapperPractice/DapperPractice/SqlContext/ConnectionOption.cs
+++ DapperPractice/DapperPractice/SqlContext/ConnectionOption.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,19 @@ namespace DapperPractice.SqlContext
 {
     public class ConnectionOption
     {
-        public static string ConnectionString { get; set; }
+        private static IDbConnection _dbConnection = new SqlConnection();
+        public static IDbConnection DbConnection
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_dbConnection.ConnectionString))
+                {
+                    _dbConnection.ConnectionString = ConnectionString;
+                }
+                return _dbConnection;
+            }
+        }
+        private static string _connectionString;
+        public static string ConnectionString { get => _connectionString; set => _connectionString = value; }
     }
 }
--- DapperPractice/DapperPractice/SqlContext/DapperHelper.cs
+++ DapperPractice/DapperPractice/SqlContext/DapperHelper.cs
@@ -10,17 +10,17 @@ namespace DapperPractice.SqlContext
 {
     public class DapperHelper
     {
-        static IDbConnection _dbConnection = new SqlConnection();
+        //static IDbConnection _dbConnection = new SqlConnection();
 
-        // 其實就是get{ return... }
-        public string ConnectionString => ConnectionOption.ConnectionString;
-        public DapperHelper()
-        {
-            if (string.IsNullOrEmpty(_dbConnection.ConnectionString))
-            {
-                _dbConnection.ConnectionString = ConnectionString;
-            }
-        }
+        //// 其實就是get{ return... }
+        //public string ConnectionString => ConnectionOption.ConnectionString;
+        //public DapperHelper()
+        //{
+        //    if (string.IsNullOrEmpty(_dbConnection.ConnectionString))
+        //    {
+        //        _dbConnection.ConnectionString = ConnectionString;
+        //    }
+        //}
 
         /// <summary>
         /// 單筆查詢
@@ -35,12 +35,12 @@ namespace DapperPractice.SqlContext
         public T QueryFirst<T>(string sql, object param = null, IDbTransaction transaction = null,
             int? commandTimeout = null, CommandType? commandType = null)
         {
-            _dbConnection.Open();
-            using (transaction = _dbConnection.BeginTransaction())
+            ConnectionOption.DbConnection.Open();
+            using (transaction = ConnectionOption.DbConnection.BeginTransaction())
             {
-                var result = _dbConnection.QueryFirstOrDefault<T>(sql, param, transaction, commandTimeout, commandType);
+                var result = ConnectionOption.DbConnection.QueryFirstOrDefault<T>(sql, param, transaction, commandTimeout, commandType);
                 transaction.Commit();
-                _dbConnection.Close();
+                ConnectionOption.DbConnection.Close();
                 return result;
             }
         }
@@ -59,13 +59,13 @@ namespace DapperPractice.SqlContext
         public IEnumerable<T> Query<T>(string sql, object param = null, IDbTransaction transaction = null,
             bool buffered = true, int? commandTimeout = null, CommandType? commandType = null)
         {
-            return _dbConnection.Query<T>(sql, param, transaction, buffered, commandTimeout, commandType);
+            return ConnectionOption.DbConnection.Query<T>(sql, param, transaction, buffered, commandTimeout, commandType);
         }
 
         public int Excute<T>(string sql, object param = null, IDbTransaction transaction = null,
            int? commandTimeout = null, CommandType? commandType = null)
         {
-            return _dbConnection.Execute(sql, param, transaction, commandTimeout, commandType);
+            return ConnectionOption.DbConnection.Execute(sql, param, transaction, commandTimeout, commandType);
         }
     }
 }
