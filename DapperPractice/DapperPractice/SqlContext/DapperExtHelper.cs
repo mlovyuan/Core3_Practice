c38462920f919e8156245f4f05bf20fa8f421a64
Yuan <mlovyuan@yahoo.com.tw>
2020-09-24 23:18:09 +0800

    2020.09.24_LearningSchedule

---
+++ DapperPractice/DapperPractice/SqlContext/DapperExtHelper.cs
@@ -0,0 +1,37 @@
+﻿using Dapper.Contrib.Extensions;
+using DapperPractice.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperPractice.SqlContext
+{
+    public class DapperExtHelper<T> where T:BaseEntity
+    {
+        public T Get(int id)
+        {
+            return ConnectionOption.DbConnection.Get<T>(id);
+        }
+        public IEnumerable<T> GetAll(int id)
+        {
+            return ConnectionOption.DbConnection.GetAll<T>();
+        }
+        public long Insert(T t)
+        {
+            return ConnectionOption.DbConnection.Insert(t);
+        }
+        public bool Update(T t)
+        {
+            return ConnectionOption.DbConnection.Update(t);
+        }
+        public bool Delete(T t)
+        {
+            return ConnectionOption.DbConnection.Delete(t);
+        }
+        public bool DeleteAll(int id)
+        {
+            return ConnectionOption.DbConnection.DeleteAll<T>();
+        }
+    }
+}
